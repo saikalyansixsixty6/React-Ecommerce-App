635590b2c5a92dc44204605b272c16233b6c7912
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-09-30 15:57:24 +0000

    add product feature created

--- Ecommerce-frontend/src/components/navbar/Navbar.jsx
+++ Ecommerce-frontend/src/components/navbar/Navbar.jsx
@@ -1,29 +1,32 @@
-import { Fragment, useContext, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { Link } from 'react-router-dom'
+import  { Fragment, useContext, useState } from 'react'
+import myContext from '../../context/data/myContext';
 import { BsFillCloudSunFill } from 'react-icons/bs'
 import { FiSun } from 'react-icons/fi'
-import myContext from '../../context/data/myContext'
+import { Link } from 'react-router-dom';
+import { Dialog, Transition } from '@headlessui/react'
 import { RxCross2 } from 'react-icons/rx'
-
-
+import { useSelector } from 'react-redux';
 
 function Navbar() {
- const context = useContext(myContext);
- const {mode,toggleMode} = context
+  const context = useContext(myContext);
+  const {mode, toggleMode} = context;
 
- const [open, setOpen] = useState(false)
- 
- const user = JSON.parse(localStorage.getItem('user'))
- const logout = () =>{
-    localStorage.clear("user");
-    window.location.href = "/login"
- }
+  const [open, setOpen] = useState(false)
 
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  // console.log(user.user.email)
+
+  const logout = () => {
+    localStorage.clear('user');
+    window.location.href = '/login'
+  }
+
+  const cartItems = useSelector((state) => state.cart)
 
   return (
     <div className='bg-white sticky top-0 z-50'>
-           <Transition.Root show={open} as={Fragment}>
+      <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
           <Transition.Child
             as={Fragment}
@@ -63,6 +66,7 @@ function Navbar() {
                   <Link to={'/allproducts'} className="text-sm font-medium text-gray-900 " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     All Products
                   </Link>
+
                   {user ? <div className="flow-root">
                     <Link to={'/order'} style={{ color: mode === 'dark' ? 'white' : '', }} className="-m-2 block p-2 font-medium text-gray-900">
                       Order
@@ -75,14 +79,15 @@ function Navbar() {
                     </Link>
                   </div> : ""}
 
-                  
-
-                  {user ? <div className="flow-root">
+                {user ? <div className="flow-root">
                     <a onClick={logout} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
                       Logout
                     </a>
-                  </div> :""}
-                  
+                  </div> : <div className="flow-root">
+                    <Link to={'/signup'}  className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Signup
+                    </Link>
+                  </div>}
                   <div className="flow-root">
                     <Link to={'/'} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer">
                       <img
@@ -109,11 +114,9 @@ function Navbar() {
         </Dialog>
       </Transition.Root>
 
-
-
-
       <header className="relative bg-white">
-        <p className="flex h-10 items-center justify-center bg-pink-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8" style={{ backgroundColor: mode === 'dark' ? 'rgb(62 64 66)' : '', color: mode === 'dark' ? 'white' : '', }}>
+        <p className="flex h-10 items-center justify-center bg-pink-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8" 
+        style={{ backgroundColor: mode === 'dark' ? 'rgb(62 64 66)' : '', color: mode === 'dark' ? 'white' : '', }}>
           Get free delivery on orders over ₹300
         </p>
 
@@ -147,16 +150,21 @@ function Navbar() {
                   <Link to={'/allproducts'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     All Products
                   </Link>
-                  <Link to={'/order'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                 {user ?  <Link to={'/order'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     Order
-                  </Link>
-                  <Link to={'/dashboard'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Admin
-                  </Link>
+                  </Link> :   <Link to={'/signup'}  className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Signup
+                    </Link>}
 
-                  <a className="text-sm font-medium text-gray-700 cursor-pointer  " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                  {user?.user?.email === 'sairohit666@gmail.com' ? 
+                   <Link to={'/dashboard'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                    Admin
+                  </Link> : ""}
+                  
+                
+                 {user ?  <a onClick={logout} className="text-sm font-medium text-gray-700 cursor-pointer  " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     Logout
-                  </a>
+                  </a> : ""}
                 </div>
 
                 <div className="hidden lg:ml-8 lg:flex">
@@ -178,13 +186,11 @@ function Navbar() {
                   </a>
                 </div>
 
-                {/* Search */}
                 <div className="flex lg:ml-6">
                   <button className='' onClick={toggleMode}>
                     {mode === 'light' ?
                       (<FiSun className='' size={30} />
-                      
-                      ) : "dark" ?
+                      ) : 'dark' ?
                         (<BsFillCloudSunFill size={30} />
                         ) : ''}
                   </button>
@@ -197,7 +203,7 @@ function Navbar() {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                     </svg>
 
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>0</span>
+                    <span className="ml-2 text-sm font-medium text-gray-700 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>{cartItems.length}</span>
                     <span className="sr-only">items in cart, view bag</span>
                   </Link>
                 </div>
@@ -209,5 +215,5 @@ function Navbar() {
     </div>
   )
 }
-    
+
 export default Navbar
--- Ecommerce-frontend/src/context/data/myState.jsx
+++ Ecommerce-frontend/src/context/data/myState.jsx
@@ -1,33 +1,99 @@
-import { useState } from "react";
-import myContext from "./myContext";
+import { useEffect, useState } from 'react'
+import MyContext from './myContext';
+import { fireDb } from '../../firebase/firebaseConfig';
+import { Timestamp, addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 
 function MyState(props) {
+  const [mode, setMode] = useState('light');  
+  const [loading, setLoading] = useState(false); 
 
- const [mode,setMode]= useState("light")
-
-
- const toggleMode = (mode) => {
-  if (mode === 'light') {
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'rgb(17, 24, 39)';
-  }
-  else {
+    }
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-
+    }
   }
-}
 
-const [loading,setLoading] = useState(false)
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString(
+      "en-US",
+      {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }
+    )
+
+  })
+
+  // ********************** Add Product Section  **********************
+  const addProduct = async () => {
+    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+      return toast.error('Please fill all fields')
+    }
+    const productRef = collection(fireDb, "products")
+    setLoading(true)
+    try {
+      await addDoc(productRef, products)
+      toast.success("Product Add successfully")
+      getProductData()
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+    setProducts("")
+  }
+
+  const [product, setProduct] = useState([]);
+
+  // ****** get product
+  const getProductData = async () => {
+    setLoading(true)
+    try {
+      const q = query(
+        collection(fireDb, "products"),
+        orderBy("time"),
+        // limit(5)
+      );
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productsArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productsArray.push({ ...doc.data(), id: doc.id });
+        });
+        setProduct(productsArray)
+        setLoading(false);
+      });
+      return () => data;
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getProductData();
+  }, []);
 
 
   return (
-    <div>
-      <myContext.Provider value={{mode,toggleMode,loading,setLoading}}>
-        {props.children}
-      </myContext.Provider>
-    </div>
+    <MyContext.Provider value={{ 
+      mode, toggleMode, loading,setLoading,
+      products, setProducts,addProduct }}>
+      {props.children}
+    </MyContext.Provider>
   )
 }
 
