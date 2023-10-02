580651c1286ff7ddf99d5ce790f9e932dd71fea7
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-10-02 10:26:03 +0000

    vercel

--- Ecommerce-frontend/vite.config.js
+++ Ecommerce-frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/react-Ecommerce-App"
+  base:"/react-Ecommerce-App",
+  build: {
+    chunkSizeWarningLimit: 800, // Set your preferred limit here
+  },
 })
